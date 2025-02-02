fdb412b7d52e29da67c327caa8770d95ae037356
Dipak Khade <dipakhade214@gmail.com>
2025-02-02 20:04:29 +0530

    first instruction init

--- favorites/programs/favorites/src/lib.rs
+++ favorites/programs/favorites/src/lib.rs
@@ -9,15 +9,22 @@ mod hello_solana{
 
     use super::*;
     // each fn is a instruction
-    pub fn init_account(ctx:Context<Init>,data:u64){
-
+    pub fn init_account(ctx:Context<Init>,data:u64) ->Result<()> {
+        ctx.accounts.new_account.data = data;
+        Ok(())
     }
 }
 
-#[derive(Account)]
+#[derive(Accounts)]
 pub struct Init <'info>{
     #[account(init,payer=signer, space = 8+8)]
     pub new_account:Account<'info,NewAccount>,
+    #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program:Program<'info,System>
+}
+
+#[account]
+pub struct NewAccount{
+    data:u64,
 }
