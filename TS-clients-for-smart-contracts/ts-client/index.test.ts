5b35ad7343a662514da707a67a9a0a25fc4026cf
Dipak Khade <dipakhade214@gmail.com>
2025-04-01 16:49:31 +0530

    test connection

--- TS-clients-for-smart-contracts/ts-client/index.test.ts
+++ TS-clients-for-smart-contracts/ts-client/index.test.ts
@@ -1,4 +1,4 @@
-import { Keypair } from "@solana/web3.js";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import * as borsh from "borsh"
 import { test, expect } from "bun:test"
 
@@ -27,6 +27,13 @@ let adminKeyPair: Keypair;
 test("counter value does change", async function () {
   adminKeyPair = Keypair.generate();
   counterAccountKeyPair = new Keypair();
+
+  // airdrop sol for new admin Keypair
+  const connection = new Connection(`http://localhost:8899`, 'confirmed');
+  const res = await connection.requestAirdrop(adminKeyPair.publicKey, 10 * LAMPORTS_PER_SOL);
+
+  const programId = new PublicKey('HZTDGsCyj5ZZ8aktu8Gfn8wEFGTvDbGidDYrLHvhH21o');
+  const rentExLamports = await connection.getMinimumBalanceForRentExemption(GERRTING_SIZE);
 })
 
-
+//HZTDGsCyj5ZZ8aktu8Gfn8wEFGTvDbGidDYrLHvhH21o
